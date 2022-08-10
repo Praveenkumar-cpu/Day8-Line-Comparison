f917aeae33979012adf542c1c32b432e52833a39
praveen <praveenpkbadiger@gmail.com>
2022-08-10 14:41:31 +0530

    Welcome to Line Comparison

---
+++ bridgelab/LineComparisonComputation.java
@@ -0,0 +1,10 @@
+package bridgelab;
+
+public class LineComparisonComputation {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Line Comparison Problem");
+
+    }
+}
